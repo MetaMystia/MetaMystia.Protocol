bf8cf694ef6d8c77c6d61273026fd63b34522915
Hz6826 <hz_2007@qq.com>
2026-06-12 15:09:57 +0800

    ci: add publish workflow

--- MetaMystia.Protocol/Transport/MpConstants.cs
+++ MetaMystia.Protocol/Transport/MpConstants.cs
@@ -6,5 +6,5 @@ public static class MpConstants
     public const int DefaultPort = 40815;
     public const int HostUid = 0;
     public const int UnassignedUid = -1;
-    public const int ProtocolVersion = 1;  // 大版本更新时千万不要忘记改！
+    public const int ProtocolVersion = 1;  // 大/小版本更新时千万不要忘记改！（修订号更改一般不用改）
 }
